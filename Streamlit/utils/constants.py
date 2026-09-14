bc1fc6d6e2f12427d77f69847f5a1796e775a5f3
MikaMahaputra <55399230+MikaMahaputra@users.noreply.github.com>
2026-09-14 16:34:25 +0700

    Update MODEL_PATH to include 'Streamlit' directory

--- Streamlit/utils/constants.py
+++ Streamlit/utils/constants.py
@@ -55,4 +55,4 @@ SAMPLE_ROW = {
 
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODEL_PATH = os.path.join(BASE_DIR, "models", "best_model.joblib")
+MODEL_PATH = os.path.join(BASE_DIR, "Streamlit", "models", "best_model.joblib")
